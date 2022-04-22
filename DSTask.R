cece677fc5763fb8d6b93295f74f48599d067542
Allan Abala <abala.allan@gmail.com>
2022-04-22 15:45:08 +0300

    Analysis and dashboard

--- DSTask.R
+++ DSTask.R
@@ -6,6 +6,7 @@
 library(lubridate)
 library(dplyr)
 library(tidyverse)
+library(tidyr)
 
 ########## Read in the data from GitHub and save it as data_all ###################################
 url <- "https://raw.githubusercontent.com/finddx/FINDCov19TrackerData/master/processed/data_all.csv"
@@ -15,28 +16,41 @@ dim(data)
 names(data)
 View(data)
 
-############ Duplicating the data to keep the original data set ####################################
+############ Duplicating the data to keep the original data set and clean data_set ####################
 data2 <- data
 attach(data2)
 View(data2)
 class(time)
+data2[is.na(data2)] <- 0
 
-############ convert the strgcharater for time to date #############################################
+############ converting character for time to date ###################################################
 data2$time<-as.Date(data2$time)
 class(data2$time)
 
-############ Creating columns for month and Quarter ################################################
-data2$month <- strftime(data2$time, "%Y-%m")
+############ Creating columns for months and quarters ################################################
+data2$monthly <- strftime(data2$time, "%Y-%m")
+data2$quarterly <- paste0(year(data2$time),
+                         "-0",
+                         quarter(data2$time))
 
+############ Calculating the number of days countries reported tests, cases and deaths ###############
+#table(data2$new_tests_orig, useNA = 'no')
+class(new_tests_orig)
+class(new_cases_orig)
+class(new_deaths_orig)
 
+data2 %>%
+  group_by(time, name, new_tests_orig) %>%
+  summarize(n = n())
 
+############ Calculating the monthly and quarterly average tests, cases and deaths ###########
 
+class(cap_new_tests)
+class(cap_new_cases)
+class(cap_new_deaths)
 
-
-
-
-
-
-
-
+monthyly_average1000 <- aggregate(cbind(cap_new_tests, cap_new_cases, cap_new_deaths) ~ monthly + 
+                                    name, data2 , mean, na.rm = TRUE)
+quartely_average1000 <- aggregate(cbind(cap_new_tests, cap_new_cases, cap_new_deaths) ~ quarterly + 
+                                    name, data2 , mean, na.rm = TRUE)
 
