d5d55d70b73c16f6b3ed667a04693b7e8f24d8eb
Allan Abala <abala.allan@gmail.com>
2022-04-22 09:29:31 +0300

    Preparing the data

--- DSTask.R
+++ DSTask.R
@@ -1,8 +1,42 @@
-########### FIND Data Science Task ################
-########### April 23, 2022 ####################
+########### FIND Data Science Task #################################################################
+########### April 23, 2022 #########################################################################
 
-# Read in the data from GitHub and save it as data_all
+########## Load Libraries ##########################################################################
+
+library(lubridate)
+library(dplyr)
+library(tidyverse)
+
+########## Read in the data from GitHub and save it as data_all ###################################
 url <- "https://raw.githubusercontent.com/finddx/FINDCov19TrackerData/master/processed/data_all.csv"
-data_all <- read.csv(url)
+data <- read.csv(url)
 
-dim(data_all)
+dim(data)
+names(data)
+View(data)
+
+############ Duplicating the data to keep the original data set ####################################
+data2 <- data
+attach(data2)
+View(data2)
+class(time)
+
+############ convert the strgcharater for time to date #############################################
+data2$time<-as.Date(data2$time)
+class(data2$time)
+
+############ Creating columns for month and Quarter ################################################
+data2$month <- strftime(data2$time, "%Y-%m")
+
+
+
+
+
+
+
+
+
+
+
+
+
