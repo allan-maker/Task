5ae950234ec778ff45a18979ac0f5bde70f4c929
Allan Abala <abala.allan@gmail.com>
2022-04-24 13:47:48 +0300

    Data Preparation and Analysis

--- DSTask.R
+++ DSTask.R
@@ -1,7 +1,7 @@
-########### FIND Data Science Task #################################################################
-########### April 23, 2022 #########################################################################
+########### FIND Data Science Task ##################################################################
+########### April 23, 2022 ##########################################################################
 
-########## Load Libraries ##########################################################################
+########## Load Libraries ###########################################################################
 
 library(lubridate)
 library(dplyr)
@@ -13,7 +13,8 @@ library(plotly)
 install.packages("pacman")
 install.packages("rcdimple")
 
-########## Read in the data from GitHub and save it as data_all ###################################
+########## Read in the data from GitHub and save it as data_all ######################################
+
 url <- "https://raw.githubusercontent.com/finddx/FINDCov19TrackerData/master/processed/data_all.csv"
 data <- read.csv(url)
 
@@ -21,7 +22,8 @@ dim(data)
 names(data)
 View(data)
 
-############ Duplicating the data to keep the original data set and clean data_set ####################
+############ Duplicating the data to keep the original data set and clean data_set ###################
+
 data2 <- data
 attach(data2)
 View(data2)
@@ -29,42 +31,52 @@ class(time)
 #data2[is.na(data2)] <- 0
 
 ############ converting character for time to date ###################################################
+
 data2$time<-as.Date(data2$time)
 class(data2$time)
 
 ############ Creating columns for months and quarters ################################################
+
 data2$monthly <- strftime(data2$time, "%Y-%m")
 data2$quarterly <- paste0(year(data2$time),
                          "-0",
                          quarter(data2$time))
 
 ############ Calculating the number of days countries reported tests, cases and deaths ###############
-#table(data2$new_tests_orig, useNA = 'no')
+
 class(new_tests_orig)
 class(new_cases_orig)
 class(new_deaths_orig)
 
+########### Number of days by Month #########################
 
-#test_days <- sum(new_tests_orig > 0 & set == "country", na.rm = T)
-#case_days <- sum(new_cases_orig > 0, na.rm = T)
-#death_days <- sum(new_deaths_orig > 0, na.rm = T)
-
-#table <- data.frame(test_days, case_days, death_days)
-
-#aggregate(x = new_tests_orig,
-         # by = list(name, data2$monthly),
-         # na.rm = T,
-        #  FUN = frequency)
-
-data2 %>%
-  group_by(monthly) %>%
+days_tests_reported_monthly <- data2 %>% group_by(monthly, name) %>%
+  filter(new_tests_orig > 0) %>%
   summarise(frequency = n())
 
-data2 %>%
-  group_by(quarterly) %>%
+days_cases_reported_monthly <- data2 %>% group_by(monthly, name) %>%
+  filter(new_cases_orig > 0) %>%
   summarise(frequency = n())
 
-############ Calculating the monthly and quarterly average tests, cases and deaths ###########
+days_deaths_reported_monthly <- data2 %>% group_by(monthly, name) %>%
+  filter(new_deaths_orig > 0) %>%
+  summarise(frequency = n())
+
+########### Number of days by Quarter  #######################
+
+days_tests_reported_quarterly <- data2 %>% group_by(quarterly, name) %>%
+  filter(new_tests_orig > 0) %>%
+  summarise(frequency = n())
+
+days_cases_reported_quarterly <- data2 %>% group_by(quarterly, name) %>%
+  filter(new_cases_orig > 0) %>%
+  summarise(frequency = n())
+
+days_deaths_reported_quarterly <- data2 %>% group_by(quarterly, name) %>%
+  filter(new_deaths_orig > 0) %>%
+  summarise(frequency = n())
+
+############ Calculating the monthly and quarterly average tests, cases and deaths ##############
 
 class(cap_new_tests)
 class(cap_new_cases)
@@ -75,8 +87,7 @@ monthyly_average1000 <- aggregate(cbind(cap_new_tests, cap_new_cases, cap_new_de
 quartely_average1000 <- aggregate(cbind(cap_new_tests, cap_new_cases, cap_new_deaths) ~ quarterly + 
                                     name, data2 , mean, na.rm = TRUE)
 
-
 ############ Boxplots ###########################################################################
 
-plot_ly(data = data2, x = set, y = cap_new_deaths, type = "box", boxpoints = "all", jitter = 0.3,
+plot_ly(data = monthyly_average1000, x = name, y = cap_new_deaths, type = "box", boxpoints = "all", jitter = 0.3,
         pointpos = -1.8)
