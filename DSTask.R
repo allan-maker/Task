2d694309fb0dc7abaacd9f1989c3b33c28236e50
Allan Abala <abala.allan@gmail.com>
2022-04-24 11:03:06 +0300

    data preparation and analysis

--- DSTask.R
+++ DSTask.R
@@ -8,6 +8,7 @@ library(dplyr)
 library(tidyverse)
 library(tidyr)
 library(billboarder)
+library(plotly)
 
 install.packages("pacman")
 install.packages("rcdimple")
@@ -25,7 +26,7 @@ data2 <- data
 attach(data2)
 View(data2)
 class(time)
-data2[is.na(data2)] <- 0
+#data2[is.na(data2)] <- 0
 
 ############ converting character for time to date ###################################################
 data2$time<-as.Date(data2$time)
@@ -43,9 +44,25 @@ class(new_tests_orig)
 class(new_cases_orig)
 class(new_deaths_orig)
 
+
+#test_days <- sum(new_tests_orig > 0 & set == "country", na.rm = T)
+#case_days <- sum(new_cases_orig > 0, na.rm = T)
+#death_days <- sum(new_deaths_orig > 0, na.rm = T)
+
+#table <- data.frame(test_days, case_days, death_days)
+
+#aggregate(x = new_tests_orig,
+         # by = list(name, data2$monthly),
+         # na.rm = T,
+        #  FUN = frequency)
+
 data2 %>%
-  group_by(time, name, new_tests_orig) %>%
-  summarize(n = n())
+  group_by(monthly) %>%
+  summarise(frequency = n())
+
+data2 %>%
+  group_by(quarterly) %>%
+  summarise(frequency = n())
 
 ############ Calculating the monthly and quarterly average tests, cases and deaths ###########
 
@@ -59,5 +76,7 @@ quartely_average1000 <- aggregate(cbind(cap_new_tests, cap_new_cases, cap_new_de
                                     name, data2 , mean, na.rm = TRUE)
 
 
+############ Boxplots ###########################################################################
 
-
+plot_ly(data = data2, x = set, y = cap_new_deaths, type = "box", boxpoints = "all", jitter = 0.3,
+        pointpos = -1.8)
